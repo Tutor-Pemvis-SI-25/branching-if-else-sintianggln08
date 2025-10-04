b1ae11ca6eb48bb1e56f10ce83c044779175b351
sintianggln08 <sintianeng0812@gmail.com>
2025-10-04 09:29:44 +0700

    PAIIIIIIIR 262626262

--- T00.java
+++ T00.java
@@ -5,10 +5,12 @@ public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int a, b;
+        int a, b, jUMLAH, kALI;
 
         a = Integer.parseInt(input.nextLine());
         b = Integer.parseInt(input.nextLine());
+        jUMLAH = a + b;
+        kALI = a * b;
         if (a % 2 == 0) {
             System.out.println("Bilangan pertama: Genap");
         } else {
@@ -29,10 +31,53 @@ public class T00 {
             }
         }
         if (a % 2 == 0 && b % 2 == 0) {
-            System.out.println("Hasil penjumlahan: " + a + b);
+            System.out.println("Hasil penjumlahan: " + jUMLAH);
         } else {
             if (a % 2 != 0 && b % 2 != 0) {
-                System.out.println("Hasil perkalian: " + a * b);
+                System.out.println("Hasil perkalian: " + kALI);
+            } else {
+                System.out.println("Berbeda jenis");
+            }
+        }
+    }
+}
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a, b, jUMLAH, kALI;
+
+        a = Integer.parseInt(input.nextLine());
+        b = Integer.parseInt(input.nextLine());
+        jUMLAH = a + b;
+        kALI = a * b;
+        if (a % 2 == 0) {
+            System.out.println("Bilangan pertama: Genap");
+        } else {
+            System.out.println("Bilangan pertama: Ganjil");
+        }
+        if (b % 2 == 0) {
+            System.out.println("Bilangan kedua: Genap");
+        } else {
+            System.out.println("Bilangan kedua: Ganjil");
+        }
+        if (a > b) {
+            System.out.println("Bilangan pertama lebih besar");
+        } else {
+            if (b > a) {
+                System.out.println("Bilangan kedua lebih besar");
+            } else {
+                System.out.println("Kedua bilangan sama besar");
+            }
+        }
+        if (a % 2 == 0 && b % 2 == 0) {
+            System.out.println("Hasil penjumlahan: " + jUMLAH);
+        } else {
+            if (a % 2 != 0 && b % 2 != 0) {
+                System.out.println("Hasil perkalian: " + kALI);
             } else {
                 System.out.println("Berbeda jenis");
             }
